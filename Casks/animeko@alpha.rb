f0edba019c73974ac0f499dcfc33b966a0de0eff
CircuitX <ou123699@gmail.com>
2026-01-26 17:25:12 +0800

    animeko@alpha 5.3.0-alpha01

--- Casks/animeko@alpha.rb
+++ Casks/animeko@alpha.rb
@@ -2,9 +2,9 @@ cask "animeko@alpha" do
     arch arm:   "aarch64",
          intel: "x86_64"
 
-    version "5.2.0-beta01"
-    sha256 arm:   "228ed4e391636f44cecaeb217661ef86bd013be6c1410660bb4ee12874c11b94",
-           intel: "866187e5bfa51e12718af2bf85719689e58ff8a12d498ba0d39bfe4f00ced531"
+    version "5.3.0-alpha01"
+    sha256 arm:   "dd942245ae4b2498b42d4262781e2a9b02613ade3c56f351e6199d39762d749e",
+           intel: "063edfad7af6e213c07ab025a86f123c73feaa6f946b415528a639dcd6f9bc46"
 
     on_arm do
         url "https://github.com/open-ani/animeko/releases/download/v#{version}/ani-#{version}-macos-#{arch}.dmg",
